1a7a16f90f844f7f446f63b1655d8bec75f9caf6
Sam Joshua <95765364+Sam-Joshua-S@users.noreply.github.com>
2023-01-05 16:49:26 +0530

    Add files via upload

---
+++ keeper-app/src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./footer";
+import Header from "./header";
+import Note from "./note";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ keeper-app/src/component/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Create a React keeper App </h1>
+      <p>learn by doing it</p>
+    </div>
+  );
+}
+
+export default Note;
